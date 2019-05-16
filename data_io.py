47a71bd6da87c1935a7fa354e8c98be5dabee398
julianebeli <julian.ebeli@education.tas.gov.au>
2019-05-16 11:35:59 +1000

    satisfactory state management, resumability

--- data_io.py
+++ data_io.py
@@ -3,35 +3,39 @@ from pathlib import Path
 
 
 class Storer:
-    def __init__(self, source_file='source.csv', store='store.py'):
-        self.source_path = Path(__init__.here) / source_file
-        self.store_path = Path(__init__.here) / store
-        self.counted_courses: set(int) = self.setup()
-        self.source_data: set(int) = self.get_source()
+    def __init__(self, source_file='source.csv', store='record'):
+        self.error_path = Path(__init__.here) / 'data' / 'error'
+        Path.touch(self.error_path)
+
+        self.source_path = Path(__init__.here) / 'data' / source_file
+        self.store_path = Path(__init__.here) / 'data' / store
+
+        self.processed: set(int) = self.setup(self.store_path)
+        self.unprocessed: set(int) = self.get_source(self.source_path)
+        self.jobs = self.diff()
 
     def diff(self):
-        return self.source_data - self.counted_courses
+        return list(self.unprocessed - self.processed)
 
-    def get_source(self):
-        with open('initial_source.csv') as f:
+    def get_source(self, file):
+        if not Path.exists(file):
+            exit('No source data found')
+        with open(file) as f:
             data = set([int(y) for y in [x.strip().split(',')[0] for x in f.readlines()[1:]]])
         return data
 
-    def setup(self):
-        with open(self.store_path, 'r') as f:
-            d = f.read()
-            if d:
-                print("stored data found")
-                process(d)
-                return set()
-            else:
-                print("no data")
-                return set()
+    def setup(self, file):
+        Path.touch(file)
+        with open(file, 'r') as f:
+            data = set([int(y) for y in [x.strip() for x in f.readlines()] if y])
+            return data
 
     def record(self, course_id):
         print(f"adding {course_id} to record")
+        with open(self.store_path, 'a') as f:
+            f.write(f"{course_id}\n")
 
     def report(self, api_result):
         print(api_result)
-
-# what's wrong here? It needs to handle the data stream breaking down
+        with open(self.error_path, 'a') as f:
+            f.write(f"{api_result}\n")
--- patcher.py
+++ patcher.py
@@ -1,18 +1,17 @@
 import __init__
 from data_io import Storer
+import json
 
 from api.requestor2 import API
-
-storer = Storer()
 
 api = API('beta')
 
 
 def update_license(course_id):
-    params = {'methodname': 'update_course', 'id': course, 'course[license]': 'cc_by'}
+    params = {'methodname': 'update_course', 'id': course_id, 'course[license]': 'cc_by'}
     api.add_method(**dict(params))
     api.do()
-    return api.results
+    return api
 
 
 def patch_courses(course_id_list):
@@ -21,10 +20,9 @@ def patch_courses(course_id_list):
         if not data.response_error:
             storer.record(course)
         else:
-            storer.report(data)
+            storer.report(json.dumps(data.results, indent=4))
 
 
 if __name__ == "__main__":
-    storer.record("3")
-    storer.report("9")
-    print(len(storer.source_data))
+    storer = Storer(source_file='initial_source.csv')
+    patch_courses(storer.jobs[:5])
